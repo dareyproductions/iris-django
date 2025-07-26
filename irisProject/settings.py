de99ed0ea123076430803447c1f08716dde04772
victorVau <darekolawolee@gmail.com>
2025-07-26 03:21:48 +0100

    allowed host

--- irisProject/settings.py
+++ irisProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+g^_2f5i#8#p%!1ql-%(uf9oik-n%334oy@#lcf7v0_vwxt$l_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
